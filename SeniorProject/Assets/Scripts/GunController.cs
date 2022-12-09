309387b469a833d551b8a3f1eabee1a6f3c37476
CalvinZhuang <c_zhuang@u.pacific.edu>
2022-12-09 06:12:06 -0800

    finished smg and assult rifle scripts and added more gun to gun game function

--- SeniorProject/Assets/Scripts/GunController.cs
+++ SeniorProject/Assets/Scripts/GunController.cs
@@ -11,6 +11,9 @@ public class GunController : MonoBehaviour
     [SerializeField] public GameObject LV2Gun;
     [SerializeField] public GameObject LV3Gun;
     [SerializeField] public GameObject LV4Gun;
+    [SerializeField] public GameObject LV5Gun;
+    [SerializeField] public GameObject LV6Gun;
+    [SerializeField] public GameObject LV7Gun;
 
     // Start is called before the first frame update
     void Start()
@@ -27,13 +30,36 @@ public class GunController : MonoBehaviour
     public void addKill()
     {
         killsCounter++;
-        Debug.Log("killcont=" + killsCounter);
         if (killsCounter == levelCap)
         {
             if (LV1Gun.activeSelf)
             {
-                Debug.Log("upgrade");
                 LV1toLV2();
+                killsCounter = 0;
+            }
+            else if (LV2Gun.activeSelf)
+            {
+                LV2toLV3();
+                killsCounter = 0;
+            }
+            else if (LV3Gun.activeSelf)
+            {
+                LV3toLV4();
+                killsCounter = 0;
+            }
+            else if (LV4Gun.activeSelf)
+            {
+                LV4toLV5();
+                killsCounter = 0;
+            }
+            else if (LV5Gun.activeSelf)
+            {
+                LV5toLV6();
+                killsCounter = 0;
+            }
+            else if (LV6Gun.activeSelf)
+            {
+                LV6toLV7();
                 killsCounter = 0;
             }
         }
@@ -43,6 +69,32 @@ public class GunController : MonoBehaviour
     {
         LV1Gun.SetActive(false);
         LV2Gun.SetActive(true);
+    }
+    
+    public void LV2toLV3()
+    {
+        LV2Gun.SetActive(false);
+        LV3Gun.SetActive(true);
+    }
+    public void LV3toLV4()
+    {
+        LV3Gun.SetActive(false);
+        LV4Gun.SetActive(true);
+    }
+    public void LV4toLV5()
+    {
+        LV4Gun.SetActive(false);
+        LV5Gun.SetActive(true);
+    }
+    public void LV5toLV6()
+    {
+        LV5Gun.SetActive(false);
+        LV6Gun.SetActive(true);
+    }
+    public void LV6toLV7()
+    {
+        LV6Gun.SetActive(false);
+        LV7Gun.SetActive(true);
     }
     
 }
