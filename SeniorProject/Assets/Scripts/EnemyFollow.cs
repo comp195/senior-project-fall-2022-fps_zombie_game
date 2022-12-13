0286f57d8b1f6aeb80bb3c0d6397e23e8b552e55
anantsingh1311 <anantsingh1311@gmail.com>
2022-12-13 11:49:29 -0800

    lvl1

--- SeniorProject/Assets/Scripts/EnemyFollow.cs
+++ SeniorProject/Assets/Scripts/EnemyFollow.cs
@@ -1,20 +1,31 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
+using TMPro;
 using UnityEngine;
 using UnityEngine.AI;
+using Vector3 = UnityEngine.Vector3;
 
 public class EnemyFollow : MonoBehaviour
 {
     public NavMeshAgent enemy;
 
     public Transform Player; // Update is called once per frame
+    private float Health = 100f;
+    [SerializeField] private TextMeshProUGUI HealthText;
 
-    public LayerMask WhatisGround, WhatisPlayer;
-    void Update()
+    public void Update()
+    {
+        EnemyChase();
+
+    }
+
+    private void EnemyChase()
     {
         enemy.SetDestination(Player.position);
-        
     }
+
     
-    
+
 }
