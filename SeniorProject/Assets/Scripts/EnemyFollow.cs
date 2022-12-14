64954a3d61270222f16583be5348a45bdd54d440
anantsingh1311 <anantsingh1311@gmail.com>
2022-12-13 17:35:50 -0800

    Zombie access problem solved

--- SeniorProject/Assets/Scripts/EnemyFollow.cs
+++ SeniorProject/Assets/Scripts/EnemyFollow.cs
@@ -14,28 +14,38 @@ public class EnemyFollow : MonoBehaviour
     [SerializeField] private GameObject enemy;
 
     private NavMeshAgent agent;
+    [SerializeField] private GameObject Pl;
 
     private float origSpeed;
     // Start is called before the first frame update
     private float maxAttackDistance = 2.0f;
     private  bool caughtPlayer;
+    
+    //PlayerHealth
+    private float Health = 100f;
+    private float curr_health;
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         caughtPlayer = false;
         origSpeed = agent.speed;
+        curr_health = Health;
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         agent.SetDestination(player.position);
         Vector3 distToPlayer = player.position - transform.position;
         if (distToPlayer.magnitude < maxAttackDistance && !caughtPlayer)
         {
             caughtPlayer = true;
-            agent.speed = 0;
-            Time.timeScale = 0;
+            curr_health -= 1;
+            if (curr_health <= 0)
+            {
+                Debug.Log("you loose");//
+                
+            }
         }
     }
 }
